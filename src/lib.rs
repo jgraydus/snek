4b726b4ed8a6aeab79e503e92d54591c4c55f6d4
Josh <josh@grayd.us>
2023-10-29 09:31:29 +0900

    rename entrance function

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub fn run() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     wasm_bindgen_futures::spawn_local(async move {
